be203674b4c929b65413f9e2deea5dc4ae9dbe73
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-06-22 10:32:57 +0000

    change content.js from a class component to a functional component using hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
+import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <NavBarSimple />*/}
       <Sidebar />
       <NavBarForm />
-      <Content />
+      {/*<Content />*/}
+      <ContentHooks />
     </div>
   );
 }
